aba20f13ef01befec1181c048911633e5fc3699c
kenNg1 <kng1230@hotmail.com>
2017-03-27 11:57:49 +0800

    add triangles.rb logic

---
+++ lib/triangles.rb
@@ -0,0 +1,18 @@
+require ('pry')
+
+class Array
+  define_method(:triangle?) do
+    sides = sort()
+    if (sides[0] + sides[1]) >= sides[2]
+      if (sides[0] == sides[1]) && (sides[1]==sides[2])
+        return "This is an equilateral triangle"
+      elsif (sides[0] == sides [1]) || (sides[1] == sides [2])
+        return "This is an isoceles triangle"
+      else
+        return "This is a scalene triangle"
+      end
+    else
+      return "This is not a triangle"
+    end
+  end
+end
