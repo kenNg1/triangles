741617e65ff3aacf0a09438ae88818e626de9002
kenNg1 <kng1230@hotmail.com>
2017-03-27 11:58:27 +0800

    add triangles.rb logic

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -3,7 +3,7 @@ require ('pry')
 class Array
   define_method(:triangle?) do
     sides = sort()
-    if (sides[0] + sides[1]) >= sides[2]
+    if (sides[0] + sides[1]) > sides[2]
       if (sides[0] == sides[1]) && (sides[1]==sides[2])
         return "This is an equilateral triangle"
       elsif (sides[0] == sides [1]) || (sides[1] == sides [2])
